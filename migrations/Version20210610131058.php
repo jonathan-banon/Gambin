662f27f11ced1e6dc7f620a97203b2f2169048db
Julien Paillassa <julien-paillassa_student2021@wilder.school>
2021-06-10 15:32:26 +0200

    lien entre user id et  commentaire produit, partie 1

--- migrations/Version20210610131058.php
+++ migrations/Version20210610131058.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210610121541 extends AbstractMigration
+final class Version20210610131058 extends AbstractMigration
 {
     public function getDescription(): string
     {

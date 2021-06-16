4c0833202cdf34db927fd17c62c26fe07460b286
banon jonathan <j.banon01@gmail.com>
2021-06-16 11:39:40 +0200

    nav fixed

--- migrations/Version20210616084619.php
+++ migrations/Version20210616084619.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210615091431 extends AbstractMigration
+final class Version20210616084619 extends AbstractMigration
 {
     public function getDescription(): string
     {

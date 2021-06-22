c7f674cd74a627d3afc0c326d208a14164ae4736
Alexisvib <alexis.vib@gmail.com>
2021-06-22 10:35:21 +0200

    fix entity

--- src/Entity/Rent.php
+++ src/Entity/Rent.php
@@ -20,12 +20,12 @@ class Rent
     private int $id;
 
     /**
-     * @ORM\Column(type="date")
+     * @ORM\Column(type="datetime")
      */
     private \DateTimeInterface $dateIn;
 
     /**
-     * @ORM\Column(type="date")
+     * @ORM\Column(type="datetime")
      */
     private \DateTimeInterface $dateOut;
 
@@ -51,7 +51,7 @@ class Rent
     private ?Status $status;
 
     /**
-     * @ORM\Column(type="date", nullable=true)
+     * @ORM\Column(type="datetime", nullable=true)
      */
     private \DateTimeInterface $dateReturn;
 
@@ -163,7 +163,7 @@ class Rent
         return $this->dateReturn;
     }
 
-    public function setDateReturn(?\DateTimeInterface $dateReturn): self
+    public function setDateReturn(\DateTimeInterface $dateReturn): self
     {
         $this->dateReturn = $dateReturn;
         return $this;
